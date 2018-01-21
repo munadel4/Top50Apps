e2bc2a30e23f489865754a5bfc74dfe9f761327a
munadel <munadel@lamsaworld.com>
2018-01-21 21:16:31 +0200

    Initial Commit

---
+++ StoreListing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreListing
+//
+//  Created by Munadel Qubbaj on 1/21/18.
+//  Copyright © 2018 Munadel Qubbaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
